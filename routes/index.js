a8005a66587ff68d951d3839666921e6d5acaae0
Mario Weidler <mario.weidler@bellin.com>
2020-11-27 13:13:22 +0100

    Display list with according icons in accordion

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,87 @@
-const {extractHostsAndApplyBlacklistFromTraefik2} = require("../routes/providersParser");
-const {extractHostsAndApplyBlacklist} = require("../routes/providersParser");
-let express = require('express');
+const {
+  extractHostsAndApplyBlacklistFromTraefik2,
+} = require("../routes/providersParser");
+const { extractHostsAndApplyBlacklist } = require("../routes/providersParser");
+let express = require("express");
 let router = express.Router();
-let request = require('request-promise-native');
-let url = require('url');
+let request = require("request-promise-native");
+let url = require("url");
+var getFavicons = require("get-website-favicon");
 
+router.get("/", function (req, res, next) {
+  let configuration = exampleConfig;
 
-router.get('/', function (req, res, next) {
-
-    let configuration = JSON.parse(process.env.ENDPOINTCONFIGURATION);
-
-    Promise.all(configuration.endpoints.map(configuration => getEndpointResult(configuration))).then(endPointView => {
-        res.render('index', {title: configuration.title, endPoints: endPointView});
+  Promise.all(
+    configuration.endpoints.map((configuration) =>
+      getEndpointResult(configuration)
+    )
+  ).then((endPointView) => {
+    res.render("index", {
+      title: configuration.title,
+      endPoints: endPointView,
     });
+  });
 });
 
 async function getEndpointResult(endPointConfiguration) {
-    let hosts = [];
-    let sectionTitle = endPointConfiguration.sectionTitle;
+  let hosts = [];
+  let sectionTitle = endPointConfiguration.sectionTitle;
 
-    try {
-        hosts = await getHostsForEndpoint(endPointConfiguration);
-        hosts = hosts.concat(getAdditionalHosts(endPointConfiguration.additionalHosts));
-        hosts = hosts.sort();
-    } catch (e) {
-        console.log(e);
-        sectionTitle += ' ( could not obtain endpoint data )';
-    }
+  try {
+    hosts = await getHostsForEndpoint(endPointConfiguration);
+    hosts = hosts.concat(
+      getAdditionalHosts(endPointConfiguration.additionalHosts)
+    );
+    hosts = hosts.sort();
+  } catch (e) {
+    console.log(e);
+    sectionTitle += " ( could not obtain endpoint data )";
+  }
 
-    return {sectionTitle: sectionTitle, hosts: hosts};
+  const enrichedHosts = await enrichHostsWithIcons(hosts);
+
+  return { sectionTitle: sectionTitle, hosts: enrichedHosts };
 }
 
 async function getHostsForEndpoint(endPointConfiguration) {
-    let hosts = [];
-    if (typeof endPointConfiguration.url !== 'undefined') {
-        let result = await request(endPointConfiguration.url);
-        hosts = extractHostsAndApplyBlacklist(result, endPointConfiguration.blacklist);
-    }
-    if (typeof endPointConfiguration.apiUrl !== 'undefined') {
-        let routersUrl = url.resolve(endPointConfiguration.apiUrl, 'http/routers?search=&status=&per_page=10000&page=1');
-        let result = await request(routersUrl);
-        hosts = extractHostsAndApplyBlacklistFromTraefik2(result, endPointConfiguration.blacklist);
-    }
-    return hosts;
+  let hosts = [];
+  if (typeof endPointConfiguration.url !== "undefined") {
+    let result = await request(endPointConfiguration.url);
+    hosts = extractHostsAndApplyBlacklist(
+      result,
+      endPointConfiguration.blacklist
+    );
+  }
+  if (typeof endPointConfiguration.apiUrl !== "undefined") {
+    let routersUrl = url.resolve(
+      endPointConfiguration.apiUrl,
+      "http/routers?search=&status=&per_page=10000&page=1"
+    );
+    let result = await request(routersUrl);
+    hosts = extractHostsAndApplyBlacklistFromTraefik2(
+      result,
+      endPointConfiguration.blacklist
+    );
+  }
+  return hosts;
 }
 
 function getAdditionalHosts(additionalHosts) {
-    if (typeof additionalHosts != 'undefined')
-        return additionalHosts;
-    else
-        return [];
+  if (typeof additionalHosts != "undefined") return additionalHosts;
+  else return [];
+}
+
+async function enrichHostsWithIcons(hosts) {
+  return Promise.all(
+    hosts.map(async (host) => {
+      var enrichedHost = { host, iconUrl: null };
+      var data = await getFavicons(host);
+      if (data && data.icons.length > 0) {
+        enrichedHost.iconUrl = data.icons[0].src;
+      }
+      return enrichedHost;
+    })
+  );
 }
 
 module.exports = router;
